7ca2970729d5650af22a2caa523088301563092f
Ricky Chien <ricky060709@gmail.com>
2015-07-11 15:16:49 +0800

    Hotfix for import css url

--- src/scripts/main.jsx
+++ src/scripts/main.jsx
@@ -1,8 +1,8 @@
 'use strict';
 
 import 'babel-core/polyfill';
-import '../styles/main.scss';
-import 'bootstrap-sass';
+import '../styles/main.css';
+import 'bootstrap';
 import React from 'react';
 import App from './components/app.jsx';
 
